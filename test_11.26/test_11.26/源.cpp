8ec1763b167b049230b4d7d8309fd65eb9ad7ccc
arryHZZ <hanzhangzhe0519@163.com>
2019-11-26 20:55:36 +0800

    练习

---
+++ test_11.26/test_11.26/源.cpp
@@ -0,0 +1,70 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "test.h"
+
+Person::Person()
+{
+	sex_ = "";
+}
+
+Person::~Person()
+{
+
+}
+
+int Person::SetValue(const string& sex)
+{
+	sex_ = sex;
+	return 0;
+}
+
+string& Person::GetValue()
+{
+	return sex_;
+}
+
+StuInfo::StuInfo()
+{
+	schoolname1_ = NULL;
+	schoolname1_ = new char[20];
+	memset(schoolname1_, '0', STUCOUNT);
+	classname1_ = NULL;
+	classname1_ = new char[20];
+	memset(classname1_, '0', STUCOUNT);
+	memset(stuname1_, '0', STUCOUNT);
+}
+
+StuInfo::~StuInfo()
+{
+	delete[] schoolname1_;
+	delete[] classname1_;
+}
+
+int StuInfo::OnInit(const char* schoolname, char* classname, const char* stuname, const string& sex)
+{
+	strcpy(schoolname1_, schoolname);
+	classname1_ = classname;
+	strcpy(stuname1_, stuname);
+	Person::SetValue(sex);
+
+	return 0;
+}
+
+
+void StuInfo::printInfo()
+{
+	printf("%s-%s-%s-%s\n", schoolname1_, classname1_, stuname1_, Person::GetValue().c_str());
+}
+
+
+int main()
+{
+	StuInfo* stu = new StuInfo();
+	const char* schname = "bite";
+	char* clasname = (char*)"60";
+	char stuname[13] = "nigulasikasi";
+	string sex = "male";
+	stu->OnInit(schname, clasname, stuname, sex);
+	stu->printInfo();
+
+	return 0;
+}
