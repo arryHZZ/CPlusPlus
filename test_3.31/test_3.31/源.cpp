e865ce4a343d69d3e544d751114cf30e420725e1
arryHZZ <hanzhangzhe0519@163.com>
2020-03-31 17:12:22 +0800

    练习

---
+++ test_3.31/test_3.31/源.cpp
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string longstr()
+
+string addstr(string str)
+{
+	auto begin = str.begin();
+	string tmp;
+	while (*begin != '\0')
+	{
+		if (*begin == '[')
+			
+	}
+}
+
+int main()
+{
+	string str;
+	addstr(str);
+	return 0;
+}
