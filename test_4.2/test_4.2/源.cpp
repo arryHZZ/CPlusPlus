aaa10ac1901344cc58a7a82b08f668827896befa
arryHZZ <hanzhangzhe0519@163.com>
2020-04-03 21:40:41 +0800

    练习

--- test_4.2/test_4.2/源.cpp
+++ test_4.2/test_4.2/源.cpp
@@ -1,32 +1,62 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-
-
-
+#include <iostream>
+using namespace std;
+//类的定义
 class X;
-class Y {
-	void g(X*);
+class Y
+{
+public:
+	void g(X*p);
 };
 class X
 {
+public:
+	X(int _a);
+	friend void Y::g(X*p);
+	friend void h(X*p);
+	friend class Z;
+	int getI() { return i; }
 private:
 	int i;
-public:
-	X(){ i = 0; }
-	friend void h(X*);
-	friend void Y::g(X*);
-	friend class Z;
 };
-void h(X* x) { x->i = +10; }
-void Y::g(X* x) { x->i++; }
-class Z {
-public:
-	void f(X* x) { x->i += 5; }
-};
-
-void main()
+class Z
 {
-	X x;
+public:
+	void f(X*p);
+};
+void h(X*p);
+//实现类函数
+X::X(int a)
+:i(a)
+{
+}
+void Y::g(X*p)
+{
+	p->i += 1;
+
+}
+void h(X*p)
+{
+	p->i += 10;
+}
+
+
+void Z::f(X*p)
+{
+	p->i += 5;
+}
+//主函数
+int main()
+{
+	X x(3);
+	Y y;
 	Z z;
+	cout << x.getI() << endl;
+	y.g(&x);
+	cout << x.getI() << endl;
 	z.f(&x);
+	cout << x.getI() << endl;
+	h(&x);
+	cout << x.getI() << endl;
+	return 0;
+
 }
--- test_4.2/test_4.2/源.hpp
+++ test_4.2/test_4.2/源.hpp
@@ -1,42 +1,62 @@
-
-#include<iostream>
+#include <iostream>
 using namespace std;
+//类的定义
+class X;
+class Y
+{
+public:
+	void g(X*p);
+};
 class X
 {
-	X(int _i)
-	:i(_i)
-	{
-	}
-	friend Z;
-	friend void h(X* p);
-	friend void Y::g(X* p);
-	friend void h(X* p);
+public:
+	X(int _a);
+	friend void Y::g(X*p);
+	friend void h(X*p);
+	friend class Z;
+	int getI() { return i; }
 private:
 	int i;
 };
-
-class Y
-{
-	void g(X* p);
-};
-
-
 class Z
 {
 public:
-	void f(X* p);
+	void f(X*p);
 };
+void h(X*p);
+//实现类函数
+X::X(int a)
+:i(a)
+{
+}
+void Y::g(X*p)
+{
+	p->i += 1;
 
-void h(X* p)
+}
+void h(X*p)
 {
 	p->i += 10;
 }
 
-void Y::g(X* p)
+
+void Z::f(X*p)
 {
-	p->i++;
+	p->i += 5;
 }
+//主函数
+int main()
+{
+	X x(3);
+	Y y;
+	Z z;
+	cout << x.getI() << endl;
+	y.g(&x);
+	cout << x.getI() << endl;
+	z.f(&x);
+	cout << x.getI() << endl;
+	h(&x);
+	cout << x.getI() << endl;
+	return 0;
 
-
-
-
+}
